08a6dbac4fcd7466560c3c1808b468ccb407ab32
Mbomain <ma.andriamparani@gmail.com>
2025-07-02 16:05:35 +0300

    fix: endpoint hello

--- src/main/java/com/example/demo/endpoint/rest/controller/hello.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/hello.java
@@ -1,9 +1,11 @@
 package com.example.demo.endpoint.rest.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller("/hello")
 public class hello {
+    @GetMapping
     public String index() {
         return "Hello, World!";
     }
